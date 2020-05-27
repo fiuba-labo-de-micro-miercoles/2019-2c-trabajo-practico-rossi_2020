9123e9ba6b0b6fe0bcecd92b2678792910a6b555
Francisco Rossi <frrossi@fi.uba.ar>
2020-05-27 10:45:54 -0300

    Entrega TP2

---
+++ tp2_rossi/asm/tp2_a.asm
@@ -0,0 +1,25 @@
+.include "m328pdef.inc"
+
+.equ entrada = 0
+.equ salida = 1
+
+.cseg
+.org 0x0000
+			JMP main
+
+.org INT_VECTORS_SIZE
+
+main:
+
+			LDI R16, (0<<0 | 1<<1) ; Configuro PB0 como entrada y PB1 como salida.
+			OUT DDRB, R16
+
+bajo: 
+			SBIS PINB,entrada ; mientras el estado de PB0 sea bajo quedara en loop. 
+			JMP bajo
+			SBI PORTB,salida ; si el valor de entrada es alto se impone un valor de 1 logico en la salida.
+alto:
+			SBIC PINB,entrada ; mientras el estado de PB0 sea alto quedara en loop.
+			JMP alto
+			CBI PORTB,salida ; si el valor de entrada es alto se impone un valor de 0 logico en la salida.
+			JMP bajo	; reinicio el ciclo
---
+++ tp2_rossi/asm/tp2_b.asm
@@ -0,0 +1,27 @@
+.include "m328pdef.inc"
+
+.equ entrada = 0
+.equ salida = 1
+
+.cseg
+.org 0x0000
+			JMP main
+
+.org INT_VECTORS_SIZE
+
+main:
+
+			LDI R16, (0<<0 | 1<<1) ; Configuro PB0 como entrada y PB1 como salida.
+			OUT DDRB, R16
+			SBI PORTB, entrada ; habilito la R de pullup interna del puerto de entrada.
+
+bajo:
+			SBIC PINB,entrada ; mientras el estado de PB0 sea alto (no presiono el botón) quedara en loop. 
+			JMP bajo
+			SBI PORTB,salida ; cuando detecto un estado en bajo de la entrada (presiono el botón) prendo el led de salida.
+
+alto: 
+			SBIS PINB,entrada ; mientras el estado de PB0 sea bajo (el botón está presionado) quedara en loop. 
+			JMP alto
+			CBI PORTB,salida ; cuando detecto un estado en alto de la entrada (suelto el botón) apago el led de salida.
+			JMP bajo ; reinicio el ciclo
